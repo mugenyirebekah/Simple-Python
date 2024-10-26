34715033a89d31d042b8ea70f8ed20ca9b030d71
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-10-26 22:46:59 +0300

    Create hello_tkinter.py

---
+++ Graphics with tkinter/hello_tkinter.py
@@ -0,0 +1,15 @@
+"""Hello World Application for Tkinter"""
+
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+label = Label(root, text="Hello World")
+label2 = Label(root, text ="Y0000000")
+button1 = Button(root, text="CLICK MEE!!!")
+button2 = Button(root, text="Don't CLICK!")
+label.pack()
+label2.pack()
+button1.pack()
+button2.pack()
+root.mainloop()
