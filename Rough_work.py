8ad44e7d3a91f5d9dcb2592dac3b43460cd4db64
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-11-28 17:01:11 +0300

    Update Rough_work.py

--- Rough_work.py
+++ Rough_work.py
@@ -591,6 +591,31 @@ def main():
 main()
 '''
 ###############################################################
+'''
 
+import string
+
+def main():
+    fileName = input("What file are the numbers in?")
+    infile = open(fileName, 'r')
+
+    sum = 0
+    count = 0
+
+    line = infile.readline()
+
+    while line != "":
+        for xStr in line.split(','):
+            sum = sum + eval(xStr)
+            count = count+1
+
+            
+        line = infile.readline()
+
+    print(sum/count)
+
+main()
+'''
+################################################################
 
 
